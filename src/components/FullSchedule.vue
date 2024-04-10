6e9f767d87cf947cf03a1077d1aa2601fe29dc5b
Kevin McAnoy <kevinmcanoy@gmail.com>
2024-04-10 11:33:39 -0700

    full schedule more legible on mobile device

--- src/components/FullSchedule.vue
+++ src/components/FullSchedule.vue
@@ -1,15 +1,40 @@
 <template>
-  <div class="col">
+  <div class="col d-lg-none">
     <h3>  {{ properDivision() }} Schedule</h3>
     <table class="table table-hover">
       <thead>
         <tr>
-          <th scope="col">Date</th>
+          <th scope="col" class="text-start d-lg-none">Date</th>
           <th scope="col">Home</th>
-          <th scope="col">Score</th>
+          <th scope="col" class="text-start d-lg-none">Sc</th>
           <th scope="col">Away</th>
-          <th scope="col">Score</th>
-          <th scope="col">Field</th>
+          <th scope="col" class="text-start d-lg-none">Sc</th>
+          <th scope="col" class="text-start d-lg-none">F</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="game in schedule" v-bind:key="game.team" :class="{ 'table-active': isSelectedTeam(game.home) || isSelectedTeam(game.away) }">
+          <td class="text-start d-lg-none">{{ displayDateSmall(game.date, game.time) }}</td>
+          <td :class="{ 'table-primary': isHomeWinner(game), 'table-info': isTied(game), 'table-danger': isHomeLoser(game)}">{{ shortTeamName(game.home) }}</td>
+          <td :class="{ 'table-primary': isHomeWinner(game), 'table-info': isTied(game), 'table-danger': isHomeLoser(game) }">{{ game.homeScore }}</td>
+          <td :class="{ 'table-primary': isAwayWinner(game), 'table-info': isTied(game), 'table-danger': isAwayLoser(game) }">{{ shortTeamName(game.away) }}</td>
+          <td :class="{ 'table-primary': isAwayWinner(game), 'table-info': isTied(game), 'table-danger': isAwayLoser(game) }">{{ game.awayScore }}</td>
+          <td>{{ shortField(game.location) }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+  <div class="col d-none d-lg-block">
+  <h3>  {{ properDivision() }} Schedule</h3>
+    <table class="table table-hover">
+      <thead>
+        <tr>
+          <th scope="col" class="text-start ">Date</th>
+          <th scope="col">Home</th>
+          <th scope="col" class="text-start">Score</th>
+          <th scope="col">Away</th>
+          <th scope="col" class="text-start">Score</th>
+          <th scope="col" class="text-star">Field</th>
         </tr>
       </thead>
       <tbody>
@@ -62,6 +87,10 @@ export default {
     displayDate(day, thetime) {
       const gameDay = this.$dayjs().year() + `/${day.split(" ")[1]} ${thetime}`;
       return this.$dayjs(gameDay, "YYYY/M/D h:mm A").format("ddd MMM DD, h:mm a")
+    },
+    displayDateSmall(day, thetime) {
+      const gameDay = this.$dayjs().year() + `/${day.split(" ")[1]} ${thetime}`;
+      return this.$dayjs(gameDay, "YYYY/M/D h:mm A").format("M/D H:mm")
     },
     shortTeamName(team) {
       const regex = / \(.*\)/i;
